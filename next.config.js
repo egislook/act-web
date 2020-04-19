94f3380485d2339226603a5978f0e7716e34c08b
EgisLook <noneedsystem@gmail.com>
2020-04-19 13:13:54 +0700

    repo assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
   webpack: config => {
     config.resolve.alias = { ...(config.resolve.alias || {}), 'react-native$': 'react-native-web' }
     config.resolve.extensions = [ '.web.js', ...config.resolve.extensions ]
+
     return config
-  }
+  },
+  assetPrefix: (process.env.NODE_ENV || 'production') === 'production' ? '/bet' : '',
 }
