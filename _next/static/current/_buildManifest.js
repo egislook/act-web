7bb1039644554964b128d7947de3f9f27a595a3b
EgisLook <noneedsystem@gmail.com>
2020-04-19 17:59:58 +0700

    build

---
+++ _next/static/current/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/alternate":[a],"/index":[a]}}("static\u002Fchunks\u002Fc6cfe01f933d1d246d716a781544825ade00a0b8.3f47bc9aa11470d8ec98.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
