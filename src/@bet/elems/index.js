048adec608ba8a64e53ca810a883a66f4168f77c
EgisLook <noneedsystem@gmail.com>
2020-04-19 17:56:02 +0700

    fixed asset prefix

--- src/@bet/elems/index.js
+++ src/@bet/elems/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import NextLink from 'next/link'
 import { StyleSheet, Text, View } from 'react-native'
-
-const assetPrefix = process.env.ASSET_PREFIX
-
-console.log({ assetPrefix })
-
 // export const Link = props => <Text accessibilityRole="link" href={assetPrefix + props.href} {...props} />
 // export const Link = (props) => <NextJsLink {...props} as={`${assetPrefix}${props.href}`} />
-export const Link = ({ href, prefetch, replace, scroll, shallow, ...rest }) => (
+export const Link = ({ href, prefetch, replace, scroll, shallow, assetPrefix = process.env.assetPrefix, ...rest }) => (
 	<NextLink as={`${assetPrefix}${href}`} href={href} passHref={true} prefetch={prefetch} replace={replace} scroll={scroll} shallow={shallow}>
 	  <Text accessibilityRole="link" {...rest} />
 	</NextLink>
