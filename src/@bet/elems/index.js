35e87d079d1448037ac5393a1abb603aa3378fb1
EgisLook <noneedsystem@gmail.com>
2020-04-19 17:03:59 +0700

    route test

--- src/@bet/elems/index.js
+++ src/@bet/elems/index.js
@@ -3,8 +3,9 @@ import NextJsLink from 'next/link'
 import { StyleSheet, Text, View } from 'react-native'
 
 const assetPrefix = process.env.ASSET_PREFIX || ''
+console.log({ assetPrefix })
 
-export const Link = props => <Text accessibilityRole="link" href={props.href} {...props} />
+export const Link = props => <Text accessibilityRole="link" href={assetPrefix + props.href} {...props} />
 // export const Link = (props) => <NextJsLink {...props} as={`${assetPrefix}${props.href}`} />
 
 export default {
